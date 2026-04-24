1b0f024471140c954dd295d6bb2e410f203ad002
10X Projects <279110288+Sistema10X@users.noreply.github.com>
2026-04-24 20:51:17 -0300

    config1

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://sistema10x.github.io/", // replace this with your deployed domain
+  author: "Sistema 10X",
+  profile: "https://github.com/Sistema10X",
+  desc: "Documentação e Anotações de Projeto da 10X.",
+  title: "Sistema 10X Docs",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -13,11 +13,11 @@ export const SITE = {
   showBackButton: true, // show back button in post detail
   editPost: {
     enabled: true,
-    text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    text: "Editar",
+    url: "https://github.com/Sistema10X/Sistema10X.github.io/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  lang: "pt-BR", // html lang code. Set this empty and default will be "en"
+  timezone: "America/Sao_Paulo", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
